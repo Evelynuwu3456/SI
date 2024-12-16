2e7661e403e65ea8b649ff882ff03fbf9ccc2e39
Evelynuwu3456 <evelyn2night3@gmail.com>
2024-12-16 20:42:11 -0300

    fffffffffffff

---
+++ Usuarios-2/src/main/java/com/example/demo/Usuarios1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usuarios1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Usuarios1Application.class, args);
+	}
+
+}
